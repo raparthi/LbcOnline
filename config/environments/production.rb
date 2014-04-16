e01bd1618a19b27d254a408a2d6c22b7e34083be
Balakishore <kishore@eggheadcreative.com>
2014-04-16 21:10:21 +0530

    minor refactoring done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.routes.default_url_options = {
+      :host => 'lbconline.herokuapp.com'
+  }
 end
